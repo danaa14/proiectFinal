dc8790fa39895f706b1fffdbc97040738de8067a
Loredana Moldovanu <loredanamoldovanu@Loredanas-MacBook-Pro.local>
2025-07-25 21:04:57 +0300

    header si tabs

---
+++ src/components/icons/header1.vue
@@ -0,0 +1,77 @@
+<script setup></script>
+
+<template>
+  <header>
+    <h1>Ratatouille</h1>
+    <nav>
+      <ul>
+        <li><a href="#">Acasă</a></li>
+        <li><a href="#">Despre noi</a></li>
+        <li><a href="#">Recenzii</a></li>
+        <li><a href="#" class="menu">Meniu</a></li>
+        <li><a href="comanda.html" class="menu">Comanda ta</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border-top: 10px solid #d1963f;
+  background-color: #474444;
+}
+
+section:nth-child(2) ul li a::after {
+  content: '';
+  width: 20px;
+  height: 30px;
+  background-image: url(img/Group\ 75.svg);
+  display: inline-block;
+  background-repeat: no-repeat;
+  vertical-align: middle;
+  margin-left: 15px;
+}
+
+.menu {
+  color: #d9d9d9;
+  background-color: #d1963f;
+  padding: 8px 16px;
+  border-radius: 16px;
+}
+
+h1 {
+  padding: 8px 16px;
+  color: #d9d9d9;
+  font-family: 'Kolker Brush';
+  font-weight: 100;
+  font-size: 96px;
+}
+
+nav {
+  width: 60%;
+}
+
+nav ul {
+  display: flex;
+  justify-content: space-around;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+nav ul li a {
+  text-decoration: none;
+  color: #d1963f;
+  font-size: 28px;
+  font-family: 'Wittgenstein';
+  transition: color 0.3s ease;
+}
+
+nav ul li a:hover {
+  color: #836635;
+}
+</style>
---
+++ src/components/tabs.vue
@@ -0,0 +1,73 @@
+<script setup></script>
+
+<template>
+  <section>
+    <div>
+      <ul>
+        <li><a href="#">Arome Clasice</a></li>
+        <li><a href="#">Tradițional</a></li>
+        <li><a href="#">Vegan</a></li>
+        <li><a href="#">Fără Gluten</a></li>
+        <li><a href="#" style="background-image: none">Recomandările Șefului</a></li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section:nth-child(2) ul li a::after {
+  content: '';
+  width: 20px;
+  height: 30px;
+  background-image: url(img/Group\ 75.svg);
+  display: inline-block;
+  background-repeat: no-repeat;
+  vertical-align: middle;
+  margin-left: 15px;
+}
+
+.menu {
+  color: #d9d9d9;
+  background-color: #d1963f;
+  padding: 8px 16px;
+  border-radius: 16px;
+}
+
+/*TABS*/
+
+section:nth-child(2) {
+  background-color: black;
+  padding: 20px;
+}
+
+section:nth-child(2) ul {
+  list-style: none;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  flex-direction: row;
+}
+
+section:nth-child(2) ul li {
+  font-size: 24px;
+  font-family: 'Wittgenstein';
+}
+
+section:nth-child(2) ul li a {
+  text-decoration: none;
+  color: #d9d9d9;
+  transition: color 0.3s ease;
+}
+
+section:nth-child(2) ul li img {
+  padding: 10px 0px;
+}
+
+section:nth-child(2) ul li a:hover {
+  color: #836635;
+}
+
+section:nth-child(2) ul li:nth-child(5) > a::after {
+  background-image: none;
+}
+</style>
